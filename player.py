Rudicito/HP-DOOM

commit 491e1003163139a08fd1edf4ce967c835b2ac652

Fix player spawn not always in a good position

player.py
@@ -6,7 +6,8 @@ class Player:
     def __init__(self, engine):
         self.engine = engine
         self.keys = self.engine.keys
-        self.thing = engine.wad_data.things[0]
+        # Player 1 start thing has a type equal to 1
+        self.thing = next((thing for thing in engine.wad_data.things if thing.type == 1), None)
         self.pos = self.thing.pos
         self.angle = self.thing.angle
         self.DIAG_MOVE_CORR = 1 / math.sqrt(2)

commit 55c760ee7c6f6e1be1f2803b653ba0996e2372e8

Fix crash on HP Prime caused by commit 491e100.
next() called with two arguments, but only one is supported on HP Prime

player.py
@@ -7,7 +7,7 @@ class Player:
         self.engine = engine
         self.keys = self.engine.keys
         # Player 1 start thing has a type equal to 1
-        self.thing = next((thing for thing in engine.wad_data.things if thing.type == 1), None)
+        self.thing = next((thing for thing in engine.wad_data.things if thing.type == 1))
         self.pos = self.thing.pos
         self.angle = self.thing.angle
         self.DIAG_MOVE_CORR = 1 / math.sqrt(2)